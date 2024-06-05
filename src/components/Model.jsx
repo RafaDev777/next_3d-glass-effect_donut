77cf4b58d5e30eac9fc24367658d439f82c7b33a
Rafael Pradita_PC <rafaeljonathan.work@gmail.com>
2024-06-05 17:54:50 +0700

    move leva to devdependecies, remove useControls

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -12,14 +12,23 @@ export default function Model() {
 		torrus.current.rotation.x += 0.02;
 	});
 
-	const materialProps = useControls({
-		thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
-		roughness: { value: 0, min: 0, max: 1, step: 0.1 },
-		transmission: { value: 1, min: 0, max: 1, step: 0.1 },
-		ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
-		chromaticAberration: { value: 0.02, min: 0, max: 1 },
-		backside: { value: true },
-	});
+	// const materialProps = useControls({
+	// 	thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
+	// 	roughness: { value: 0, min: 0, max: 1, step: 0.1 },
+	// 	transmission: { value: 1, min: 0, max: 1, step: 0.1 },
+	// 	ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
+	// 	chromaticAberration: { value: 0.02, min: 0, max: 1 },
+	// 	backside: { value: true },
+	// });
+
+	const materialProps = {
+		thickness: 0.2,
+		roughness: 0,
+		transmission: 1,
+		ior: 1.2,
+		chromaticAberration: 0.02,
+		backside: true,
+	};
 
 	return (
 		<group scale={viewport.width / 3.75}>
